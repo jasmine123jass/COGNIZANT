c6c352df3a44a78dfb487e9fff9a96ac0b361442
sruji <srujithajasmineb@gmail.com>
2026-07-10 22:45:13 +0530

    Completed Spring REST Exercise 2 - Country Web Service

---
+++ Spring REST using Spring Boot 3/Exercise 2 - REST - Country Web Service/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.model.Country;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountry() {
+
+        return new Country("IN", "India");
+
+    }
+
+}
