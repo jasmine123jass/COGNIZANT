a2d508dd576fee780ae1ecb1a43ace2b6f9238d4
sruji <srujithajasmineb@gmail.com>
2026-07-18 20:31:36 +0530

    Implemented player listing and score filtering

--- React/ReactJS-HOL 9/cricketapp/src/App.js
+++ React/ReactJS-HOL 9/cricketapp/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import ListofPlayers from "./Components/ListofPlayers";
+import Scorebelow70 from "./Components/Scorebelow70";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const players = [
+    { name: "Jack", score: 50 },
+    { name: "Michael", score: 70 },
+    { name: "John", score: 40 },
+    { name: "Ann", score: 61 },
+    { name: "Elisabeth", score: 61 },
+    { name: "Sachin", score: 95 },
+    { name: "Dhoni", score: 100 },
+    { name: "Virat", score: 84 },
+    { name: "Jadeja", score: 64 },
+    { name: "Raina", score: 75 },
+    { name: "Rohit", score: 80 }
+  ];
+
+  const flag = true;
+
+  if (flag === true) {
+
+    return (
+      <div>
+
+        <h1>List of Players</h1>
+
+        <ListofPlayers players={players} />
+
+        <hr />
+
+        <h1>List of Players having Scores Less than 70</h1>
+
+        <Scorebelow70 players={players} />
+
+      </div>
+    );
+  }
+
+  return <div></div>;
 }
 
 export default App;
