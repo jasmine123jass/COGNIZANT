09ba55c225be9cd90b2d7371a0827e760be9029c
sruji <srujithajasmineb@gmail.com>
2026-07-22 09:40:59 +0530

    Replaced App.js

--- React/ReactJS-HOL 13/bloggerapp/src/App.js
+++ React/ReactJS-HOL 13/bloggerapp/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import BookDetails from "./Components/BookDetails";
+import BlogDetails from "./Components/BlogDetails";
+import CourseDetails from "./Components/CourseDetails";
+
+import { books, blogs, courses } from "./Components/Data";
 
 function App() {
+
+  const showBooks = true;
+  const showBlogs = true;
+  const showCourses = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "flex-start",
+        marginTop: "40px"
+      }}
+    >
+
+      {showCourses && (
+        <div
+          style={{
+            borderRight: "3px solid green",
+            paddingRight: "40px"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CourseDetails courses={courses} />
+        </div>
+      )}
+
+      {showBooks && (
+        <div
+          style={{
+            borderRight: "3px solid green",
+            padding: "0 40px"
+          }}
+        >
+          <BookDetails books={books} />
+        </div>
+      )}
+
+      {showBlogs && (
+        <div style={{ paddingLeft: "40px" }}>
+          <BlogDetails blogs={blogs} />
+        </div>
+      )}
+
     </div>
+
   );
 }
 
